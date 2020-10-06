ab006d617fc6450fcc682738937cd0fdcb1f2ed3
Josue Narro <josue.narro.a@gmail.com>
2020-10-06 18:47:53 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */
+import { Global, jsx, css } from "@emotion/core";
+import normalize from "normalize.css";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Global
+        styles={css`
+          ${normalize};
+          @import url("https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap");
+          body {
+            background-color: whitesmoke;
+            font-family: "Press Start 2P", cursive;
+          }
+        `}
+      />
+      <div className="App">
+        <h1>Pokedex</h1>
+      </div>
+    </React.Fragment>
   );
 }
 
